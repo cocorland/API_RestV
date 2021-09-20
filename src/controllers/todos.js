10e56bc14b6709fabb7dca1701fc559c9f19d5b4
Administrador Venceramica <adminvencer@Venceramica.com>
2021-09-20 14:26:52 -0400

    Se resuelven los conflictos de CORS policy

--- src/controllers/todos.js
+++ src/controllers/todos.js
@@ -4,13 +4,14 @@ const fs = require('file-system');
 //const directorios = require('../maintain.json');
 const dirTree = require("directory-tree");
 const directorios = dirTree("/Users/adminvencer/Documents/PasantiaOrlando/");
+const todos = [directorios];
 
 const _ = require('underscore');
 
 //Endpoints:
 //En api/directorios estoy mostrando el json que contiene todas mis carpetas, incluyendo subcarpetas y archivos
 router.get('/', (req, res) => {
-    res.json(directorios);
+    res.json(todos);
 });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -10,10 +10,29 @@ app.set('json spaces', 2);
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+//Pray
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // routes
 app.use('/api/pdfs', require('./routes/pdfs'));
-app.use('/api/directorios', require('./controllers/directorios'));
+app.use('/api/todos', require('./controllers/todos'));
 
 //starting de server
 app.listen(app.get('port'), () => {
