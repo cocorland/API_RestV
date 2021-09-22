57daec3ea3e3ca84455a3ac22a302d6f45694cdb
Administrador Venceramica <adminvencer@Venceramica.com>
2021-09-22 14:32:34 -0400

    Se modifican elementos para que el front refresque la peticion a la API

--- src/controllers/todos.js
+++ src/controllers/todos.js
@@ -1,16 +1,14 @@
 const { Router } = require('express');
 const router = Router();
 const fs = require('file-system');
-//const directorios = require('../maintain.json');
-const dirTree = require("directory-tree");
-const directorios = dirTree("/Users/adminvencer/Documents/PasantiaOrlando/");
-const todos = [directorios];
-
 const _ = require('underscore');
 
 //Endpoints:
 //En api/directorios estoy mostrando el json que contiene todas mis carpetas, incluyendo subcarpetas y archivos
 router.get('/', (req, res) => {
+    const dirTree = require("directory-tree");
+    const directorios = dirTree("/Users/adminvencer/Documents/PasantiaOrlando/");
+    const todos = [directorios];
     res.json(todos);
 });
 
